6db05e54e8b206ed125395d76c0469b3bc3cd4b6
shazwan <shazwanx23@gmail.com>
2017-01-28 15:48:01 +0800

    simple angular2 website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,21 @@
-import { NgModule } from '@angular/core'
-import { RouterModule } from '@angular/router';
-import { rootRouterConfig } from './app.routes';
-import { AppComponent } from './app.component';
-import { GithubService } from './github/shared/github.service';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
+import { NgModule } 		from '@angular/core';
+import { BrowserModule } 	from '@angular/platform-browser';
 
-import { AboutComponent } from './about/about.component';
-import { HomeComponent } from './home/home.component';
-import { RepoBrowserComponent } from './github/repo-browser/repo-browser.component';
-import { RepoListComponent } from './github/repo-list/repo-list.component';
-import { RepoDetailComponent } from './github/repo-detail/repo-detail.component';
-import { LocationStrategy, HashLocationStrategy } from '@angular/common';
-import { ContactComponent } from './contact/contact.component';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
+import { HomeComponent } from './components/pages/home.component';
+import { AboutComponent } from './components/pages/about.component';
+import { routing } from './app.routing';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AboutComponent,
-    RepoBrowserComponent,
-    RepoListComponent,
-    RepoDetailComponent,
-    HomeComponent,
-    ContactComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpModule,
-    RouterModule.forRoot(rootRouterConfig, { useHash: true })
-  ],
-  providers: [
-    GithubService
-  ],
-  bootstrap: [ AppComponent ]
+	imports: [BrowserModule, routing], 
+	declarations: [ AppComponent,
+					NavbarComponent,
+					JumbotronComponent,
+					HomeComponent, 
+					AboutComponent 
+					],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule {
-
-}
+export class AppModule { }
